4ddceb9557109731720785b64c857ab1fcd8c48a
Matthew Sunner <matthew@matthews-mbp.lan>
2021-01-13 21:22:24 -0600

    Delete DB; Update Pipeline; Update Ignore Log
    Delete the SQLite3 database and implement MySQL database for production.
    Update pipeline functions and convert previously procedural code to
    functional code. Update the ignore log.

--- pipeline-app/pipeline.py
+++ pipeline-app/pipeline.py
@@ -1,25 +1,104 @@
-from sqlite3 import connect
+import json
+import time
+from dateutil import parser
+
+import mysql.connector as mysql
 from configparser import ConfigParser
 from pathlib import Path
 
 import tweepy
 
-with connect('airflowdb.db') as conn:
-    cur = conn.cursor()
-    print(cur)
+# Connect to Twitter & Authenticate with Tweepy
 
 
-CONFIG_FILE = Path.cwd() / "config.cfg"
+def twitter_connect(config_file):
+    """twitter_connect: Connect to Twitter client using .cfg config file
 
-config = ConfigParser()
-config.read(CONFIG_FILE)
+    Returns:
+        obj: Twitter/Tweepy auth object
+    """
 
-auth = tweepy.OAuthHandler(config.get("twitter", "consumer_key"), config.get("twitter", "consumer_secret"))
-auth.set_access_token(config.get("twitter", "access_token"), config.get("twitter", "access_token_secret"))
+    CONFIG_FILE = Path.cwd() / f"{config_file}"
 
-twitter = tweepy.API(auth)
+    config = ConfigParser()
+    config.read(CONFIG_FILE)
 
-public_tweets = twitter.home_timeline()
+    auth = tweepy.OAuthHandler(config.get(
+        "twitter", "consumer_key"), config.get("twitter", "consumer_secret"))
+    auth.set_access_token(config.get("twitter", "access_token"),
+                          config.get("twitter", "access_token_secret"))
 
-for tweet in public_tweets:
-    print(tweet.text)
+    twitter = tweepy.API(auth, wait_on_rate_limit=True,
+                         wait_on_rate_limit_notify=True)
+
+    return twitter
+
+
+# Connect to Database (MySQL)
+# TODO: Move 'localhost' results to a dotenv file for deployment to prod.
+
+
+def connect_db(database):
+    """connect_db: Connect to a given database, based on input params
+
+    Args:
+        database(dict): Dict of MySQL connection details
+
+    Returns:
+        dbconnect: MySql database connection object
+    """
+    try:
+        dbconnect = mysql.connect(
+            host=database.get("host"),
+            user=database.get("user"),
+            password=database.get("password"),
+            db=database.get("db"),
+        )
+        return dbconnect
+    except mysql.Error as e:
+        print(e)
+
+
+def create_table(database, table_name):
+    """create_table: Create a new MySQL table
+
+    Args:
+        database (dict): Dict of MySQL connection details
+        table_name (str): String of new table name 
+    """
+
+    dbconnect = connect_db(database)
+    cursor = dbconnect.cursor()
+
+    # Update the SQL statement based on needed database
+    cursor.execute("USE twitterdb")
+
+    cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+
+    query = (
+        f"CREATE TABLE `{table_name}` ("
+        "  `id` INT(11) NOT NULL AUTO_INCREMENT,"
+        "  `user` varchar(100) NOT NULL ,"
+        "  `created_at` timestamp,"
+        "  `tweet` varchar(255) NOT NULL,"
+        "  `retweet_count` int(11) ,"
+        "  `id_str` varchar(100),"
+        "  PRIMARY KEY (`id`))"
+    )
+
+    cursor.execute(query)
+    dbconnect.close()
+    cursor.close()
+
+
+def main():
+    DATABASE = {
+        'host': 'localhost',
+        'user': 'root',
+        'password': '',
+        'db': 'twitterdb'
+    }
+
+
+if __name__ == "__main__":
+    main()
