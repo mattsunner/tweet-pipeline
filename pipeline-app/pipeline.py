e41acc4430e6f36ed4d33f104206033473dc7a56
Matthew Sunner <matthewsunner@matthews-mbp.lan>
2021-01-18 08:30:25 -0600

    Finalize Tweet Streaming
    Finalize the streaming script for real time streaming into MySQL database.

--- pipeline-app/pipeline.py
+++ pipeline-app/pipeline.py
@@ -1,24 +1,41 @@
 import json
 import time
 from dateutil import parser
+import os
 
 import mysql.connector as mysql
 from configparser import ConfigParser
 from pathlib import Path
 
 import tweepy
+from dotenv import load_dotenv
+
+# Env Variable Loading
+load_dotenv()
+
+HOST = os.getenv("HOST")
+USER = 'root'  # TODO: Troubleshoot 'root' env variable loading in as main user
+PASSWORD = os.getenv("PASSWORD")
+DB = os.getenv("DB")
+
+DATABASE = {
+    'host': HOST,
+    'user': USER,
+    'password': PASSWORD,
+    'db': DB
+}
+
+CONFIG_FILE = Path.cwd() / "config.cfg"
 
 # Connect to Twitter & Authenticate with Tweepy
 
 
-def twitter_connect(config_file):
+def twitter_connect():
     """twitter_connect: Connect to Twitter client using .cfg config file
 
     Returns:
         obj: Twitter/Tweepy auth object
     """
-
-    CONFIG_FILE = Path.cwd() / f"{config_file}"
 
     config = ConfigParser()
     config.read(CONFIG_FILE)
@@ -46,15 +63,9 @@ class CustomListener(tweepy.StreamListener):
         return True
 
     def on_data(self, data):
-        """
-        Automatic detection of the kind of data collected from Twitter
-        This method reads in tweet data as JSON and extracts the data we want.
-        """
         try:
-            # parse as json
             raw_data = json.loads(data)
 
-            # extract the relevant data
             if "text" in raw_data:
                 user = raw_data["user"]["screen_name"]
                 created_at = parser.parse(raw_data["created_at"])
@@ -62,15 +73,14 @@ class CustomListener(tweepy.StreamListener):
                 retweet_count = raw_data["retweet_count"]
                 id_str = raw_data["id_str"]
 
-            # insert data just collected into MySQL my_database
-            populate_table(user, created_at, tweet, retweet_count, id_str)
+            populate_table(user, created_at, tweet,
+                           retweet_count, id_str)
             print(f"Tweet colleted at: {created_at}")
 
-        except Error as e:
+        except Exception as e:
             print(e)
 
 # Connect to Database (MySQL)
-# TODO: Move 'localhost' results to a dotenv file for deployment to prod.
 
 
 def connect_db(database):
@@ -106,9 +116,7 @@ def create_table(database, table_name):
     dbconnect = connect_db(database)
     cursor = dbconnect.cursor()
 
-    # Update the SQL statement based on needed database
     cursor.execute("USE twitterdb")
-
     cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
 
     query = (
@@ -127,13 +135,67 @@ def create_table(database, table_name):
     cursor.close()
 
 
+def populate_table(
+    user, created_at, tweet, retweet_count, id_str, my_database=DATABASE
+):
+    """Populate table with collected data from Twitter
+
+    Args:
+        user (str): username from the status
+        created_at (datetime): when the tweet was created
+        tweet (str): text
+        retweet_count (int): number of retweets
+        id_str (int): unique id for the tweet
+    """
+
+    dbconnect = connect_db(my_database)
+
+    cursor = dbconnect.cursor()
+    cursor.execute("USE twitterdb")
+
+    query = "INSERT INTO tweets(user, created_at, tweet, retweet_count, id_str) VALUES(%s,%s,%s,%s,%s)"
+
+    args = (user, created_at, tweet, retweet_count, id_str)
+
+    try:
+        cursor.execute(query, args)
+        dbconnect.commit()
+        print("commited")
+
+    except mysql.Error as e:
+        print(e)
+        dbconnect.rollback()
+
+    cursor.close()
+    dbconnect.close()
+
+    return
+
+
+def start_stream(stream, **kwargs):
+    """Start the stream, prints the disconnection error
+
+    Args:
+        stream (obj): stream object to start
+    """
+
+    try:
+        stream.filter(**kwargs)
+    except Exception:
+        stream.disconnect()
+        print("Fatal exception")
+
+
 def main():
-    DATABASE = {
-        'host': 'localhost',
-        'user': 'root',
-        'password': '',
-        'db': 'twitterdb'
-    }
+    create_table(DATABASE, 'tweets')
+
+    twitter = twitter_connect()
+
+    StreamListener = CustomListener()
+    tweetStream = tweepy.Stream(
+        auth=twitter.auth, listener=StreamListener, timeout=30)
+
+    start_stream(tweetStream, track=["python"])
 
 
 if __name__ == "__main__":
